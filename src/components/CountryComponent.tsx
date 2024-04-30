c0ef9cadb125adb0a65bcc86f9c9068d29d38b7c
julian-pacilio <julian.pacilio.info@gmail.com>
2024-04-30 20:09:06 -0300

    style: Format Code

--- src/components/CountryComponent.tsx
+++ src/components/CountryComponent.tsx
@@ -7,24 +7,24 @@ export default function Country ({ code, name, image, imageAlt, population, regi
 
     return(
         <>
-                <Link to={`/country/${code}`}>
-                    <article className="max-w-xs shadow-md rounded-md">
-                        <picture className="block">
-                            <img className="rounded-tr-md rounded-tl-md h-[180px] w-[320px] object-cover"
-                                src={image}
-                                alt={imageAlt} 
-                            />
-                        </picture>
-                        <div className="px-5 pt-8 pb-12">
-                            <h2 className="mb-4 font-bold text-xl">{name}</h2>
-                            <ul className="flex flex-col gap-2">
-                                <li><b>Population:</b> { new Intl.NumberFormat('es-ES').format(population) }</li>
-                                <li><b>Region:</b> {region}</li>
-                                <li><b>Capital:</b> {capital}</li>
-                            </ul>
-                        </div>
-                    </article>
-                </Link>
+            <Link to={`/country/${code}`}>
+                <article className="card max-w-xs shadow-md rounded-md">
+                    <picture className="block">
+                        <img className="rounded-tr-md rounded-tl-md h-[180px] w-[320px] object-cover"
+                            src={image}
+                            alt={imageAlt} 
+                        />
+                    </picture>
+                    <div className="px-5 pt-8 pb-12">
+                        <h2 className="mb-4 font-bold text-xl">{name}</h2>
+                        <ul className="flex flex-col gap-2">
+                            <li><b>Population:</b> { new Intl.NumberFormat('es-ES').format(population) }</li>
+                            <li><b>Region:</b> {region}</li>
+                            <li><b>Capital:</b> {capital}</li>
+                        </ul>
+                    </div>
+                </article>
+            </Link>
         </>
     )
 }
