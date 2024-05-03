ff384b7f8a274d43cb59792e2b323d6eb249e467
julian-pacilio <julian.pacilio.info@gmail.com>
2024-05-03 01:20:23 -0300

    update: Refactor Router Structure & Fix Bug With Ligth/Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
-import Header  from "./components/HeaderComponent"
-import { SearchComponent } from "./components/SearchComponent"
+import Header from "./components/HeaderComponent"
+import { RouterProvider, Outlet, createBrowserRouter } from "react-router-dom"
+
+import Country from './pages/CountryDetails';
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainApp/>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '',
+        element: <HomePage/>,
+      },
+      {
+        path: 'country',
+        element: <Outlet/>,
+        children: [
+          {
+            path: ':code',
+            element: <Country/>,
+          }
+        ]
+      }
+    ]
+  }
+]);
 
 export default function App() {
-
     return (
         <>
-            <Header/>
-            <main className="max-w-[1440px] mx-auto my-10 px-5">
-                <SearchComponent></SearchComponent>
-            </main>
+            <RouterProvider router={router} />
         </>
     )
+}
+
+export function MainApp() {
+    return(
+        <>
+            <Header />
+            <main className="max-w-[1440px] mx-auto my-10 px-5">
+                <Outlet />
+            </main>
+        </>
+  )
 }
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import { SearchComponent } from "../components/SearchComponent"
+
+export default function HomePage() {
+
+    return(
+        <>
+            <SearchComponent></SearchComponent>
+        </>
+    )
+}
