2c24045d33053661c7df1e29f4cc9e1a019835af
julian-pacilio <julian.pacilio.info@gmail.com>
2024-05-07 21:46:09 -0300

    update: Custom Shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom-shadow' : '0px 0px 6px 3px rgba(0,0,0,0.1)',
+      }
+    },
   },
   plugins: [],
 }
