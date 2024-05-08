c170f65db580f87cefaa0464808890faac202ca6
julian-pacilio <julian.pacilio.info@gmail.com>
2024-05-07 21:42:27 -0300

    update: Add Code Country To Results Array

--- src/helpers/ProcessBorder.js
+++ src/helpers/ProcessBorder.js
@@ -5,7 +5,7 @@ export default async function ProcessBorder(borders) {
     await Promise.all(borders.map(async (code) => {
         const response = await fetch(`https://restcountries.com/v3.1/alpha/${code}?fields=name,common`);
         const data = await response.json();
-        results.push(data.name.common);
+        results.push([data.name.common, code]);
     }));
     
     return results;
